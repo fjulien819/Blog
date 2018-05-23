0f1ed4b7b4cfe8f10741ac1300c04bc4fe0c88c0
Julien <fjulien819@gmail.com>
2018-05-23 10:27:51 +0200

    Intégration de la gestion des sessions dans le framework :
    - Ajout dans le dossier Framework d'une classe Session qui modélise une
    session
    - Ajout dans la classe Requete :
    D'un require vers classe Session.
    D'un attribut pour l'instance de Session.
    Instanciation de Session dans le constructeur.
    D'une methode getSession.

--- Framework/Requete.php
+++ Framework/Requete.php
@@ -1,8 +1,8 @@
 <?php
-
+require_once 'Session.php';
 /*
  * Classe modélisant une requête HTTP entrante
- * 
+ *
  * @version 1.0
  * @author Baptiste Pesquet
  */
@@ -10,21 +10,24 @@ class Requete {
 
     /** Tableau des paramètres de la requête */
     private $parametres;
+    /** Objet session associé à la requête */
+    private $session;
 
     /**
      * Constructeur
-     * 
+     *
      * @param array $parametres Paramètres de la requête
      */
     public function __construct($parametres) {
         $this->parametres = $parametres;
+        $this->session = new Session();
     }
 
     /**
      * Renvoie vrai si le paramètre existe dans la requête
-     * 
+     *
      * @param string $nom Nom du paramètre
-     * @return bool Vrai si le paramètre existe et sa valeur n'est pas vide 
+     * @return bool Vrai si le paramètre existe et sa valeur n'est pas vide
      */
     public function existeParametre($nom) {
         return (isset($this->parametres[$nom]) && $this->parametres[$nom] != "");
@@ -32,7 +35,7 @@ class Requete {
 
     /**
      * Renvoie la valeur du paramètre demandé
-     * 
+     *
      * @param string $nom Nom d paramètre
      * @return string Valeur du paramètre
      * @throws Exception Si le paramètre n'existe pas dans la requête
@@ -46,5 +49,13 @@ class Requete {
         }
     }
 
-}
+    /**
+     * Renvoie l'objet session associé à la requête
+     *
+     * @return Session Objet session
+     */
+    public function getSession() {
+        return $this->session;
+    }
 
+}
