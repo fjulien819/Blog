cb9f4e677522377dba35139e1711275a76cfa23a
Julien <fjulien819@gmail.com>
2018-06-07 11:23:48 +0200

    Ajout de tinyMCE pour l'ajout et la modification d'un billet

--- Vue/Admin/addBillet.php
+++ Vue/Admin/addBillet.php
@@ -1,11 +1,11 @@
 <form method="post" action="admin/add">
   <div class="form-group">
      <label for="exampleInputPassword1">Titre : </label>
-     <input type="text" class="form-control" id="exampleInputPassword1" name="titre">
+     <input type="text" class="form-control" id="exampleInputPassword1" name="titre" required>
   </div>
   <div class="form-group">
-      <label for="exampleFormControlTextarea1">Contenu :</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" name="contenu"  rows="3"></textarea>
+      <label for="addbillet">Contenu :</label>
+      <textarea class="form-control textareaBillet" id="addbillet" name="contenu"  rows="3" required></textarea>
   </div>
   <input type="hidden" name="id" />
 
--- Vue/Admin/updateBillet.php
+++ Vue/Admin/updateBillet.php
@@ -5,8 +5,8 @@
      <input type="text" class="form-control" id="exampleInputPassword1" name="titre" value="<?php echo $billet['titre']; ?>">
   </div>
   <div class="form-group">
-      <label for="exampleFormControlTextarea1">Contenu :</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" name="contenu"  rows="3"><?php echo $billet['contenu']; ?></textarea>
+      <label for="updatebillet">Contenu :</label>
+      <textarea class="form-control textareaBillet" id="updatebillet" name="contenu"  rows="3"><?php echo $billet['contenu']; ?></textarea>
   </div>
   <input type="hidden" name="id" value="<?= $billet['id'] ?>" />
   <a class="btn btn-dark" href="Admin/billets" role="button">Annuler les modifications</a>
